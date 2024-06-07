d7a8e9110a7c780896d70fe43de6e16bfc19999f
simlav000 <simon.lavoie@mail.mcgill.ca>
2024-06-07 19:35:41 -0400

    new neovim configs

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,3 +3,12 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require('telescope').setup {
+    defaults = {
+        file_picker = {
+            prompt_title = 'All Files',
+            find_command = {'rg', '--files', '--hidden'}
+        }
+    }
+}

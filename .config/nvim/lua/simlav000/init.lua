f1812552f1a0d5f2c5efa92e37485193e3eedfa0
simlav000 <simon.lavoie@mail.mcgill.ca>
2024-06-13 17:32:30 -0400

    Using kickstart its rly good

--- .config/nvim/lua/simlav000/init.lua
+++ .config/nvim/lua/simlav000/init.lua
@@ -1,30 +1,29 @@
 require("simlav000.set")
-require("simlav000.packer")
 require("simlav000.remap")
 
 -- Create a namespace for the extmarks
-local ns_id = vim.api.nvim_create_namespace('multiple_cursors')
+local ns_id = vim.api.nvim_create_namespace("multiple_cursors")
 
 -- Function to place cursor-like marks in front of each line in visual block mode
 function place_cursors_visual_block()
-    -- Get the start and end lines of the visual block
-    local start_line = vim.fn.line("'<")
-    local end_line = vim.fn.line("'>")
+	-- Get the start and end lines of the visual block
+	local start_line = vim.fn.line("'<")
+	local end_line = vim.fn.line("'>")
 
-    -- Clear existing marks in the namespace
-    vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
+	-- Clear existing marks in the namespace
+	vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
 
-    -- Loop through each line in the visual block
-    for line = start_line, end_line do
-        -- Place a cursor-like mark at the beginning of the line
-        local buf = vim.fn.bufnr('')
-        vim.api.nvim_buf_set_extmark(buf, ns_id, line - 1, 0, {
-            virt_text = {{' ', 'Comment'}}
-        })
-    end
-    -- Enter insert mode
-    vim.cmd('startinsert')
+	-- Loop through each line in the visual block
+	for line = start_line, end_line do
+		-- Place a cursor-like mark at the beginning of the line
+		local buf = vim.fn.bufnr("")
+		vim.api.nvim_buf_set_extmark(buf, ns_id, line - 1, 0, {
+			virt_text = { { " ", "Comment" } },
+		})
+	end
+	-- Enter insert mode
+	vim.cmd("startinsert")
 end
 
 -- Keybinding to trigger the function in visual mode
-vim.api.nvim_set_keymap('x', 'I', ':lua place_cursors_visual_block()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "I", ":lua place_cursors_visual_block()<CR>", { noremap = true, silent = true })
--- .config/nvim/lua/simlav000/remap.lua
+++ .config/nvim/lua/simlav000/remap.lua
@@ -1,26 +1,11 @@
 vim.g.mapleader = " "
 
 -- Insert mode bindings
-vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true })
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
 
 -- Normal mode bindings
-vim.api.nvim_set_keymap('n', 'sn', ':set invnumber<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'sp', ':set invpaste<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'sf', ':set invfoldenable<CR>', { noremap = true })
+vim.api.nvim_set_keymap("n", "sn", ":set invnumber<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "sp", ":set invpaste<CR>", { noremap = true })
 
 -- Visual mode bindings
-vim.api.nvim_set_keymap('v', '<C-c>', ':w !xclip -i -sel c<CR><CR>', { noremap = true })
-
--- Set fold method and markers
-vim.o.foldmethod = 'marker'
-vim.o.foldmarker = '/*,*/'
-
--- Toggle fold function
-function ToggleFold()
-  if vim.fn.foldclosed(vim.fn.line('.')) == -1 then
-    vim.cmd('normal! zc')
-  else
-    vim.cmd('normal! zo')
-  end
-end
-
+vim.api.nvim_set_keymap("v", "<C-c>", ":w !xclip -i -sel c<CR><CR>", { noremap = true })
